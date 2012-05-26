e192472940d3249c80e60dad0dc5002a537fbe41
Joakim Uddholm <joakim@uddholm.com>
2012-05-26 17:37:07 +0200

    First version with Installer

---
+++ PasswordGenerator/Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using PasswordLib;
+
+namespace PasswordGenerator
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            textboxPassword.Text = Password.GeneratePassword((int) numericLength.Value); 
+        }
+
+        private void buttonCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textboxPassword.Text);
+
+        }
+    }
+}
